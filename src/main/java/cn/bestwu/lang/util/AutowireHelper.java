c9834e8a1c588c62d2fedf3d4f1099894e81691e
Peter Wu <piterwu@outlook.com>
2017-02-07 10:09:29 +0800

    增加getProperty 重载方法

--- src/main/java/cn/bestwu/lang/util/AutowireHelper.java
+++ src/main/java/cn/bestwu/lang/util/AutowireHelper.java
@@ -1,12 +1,11 @@
 package cn.bestwu.lang.util;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.core.env.Environment;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * bean 导入工具
@@ -15,51 +14,105 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class AutowireHelper implements ApplicationContextAware {
 
-	private static ApplicationContext applicationContext;
+  private static ApplicationContext applicationContext;
 
-	private static Environment environment;
+  private static Environment environment;
 
-	/**
-	 * getProperty
-	 *
-	 * @param key key
-	 * @return Property
-	 */
-	public static String getProperty(String key) {
-		return environment.getProperty(key);
-	}
+  /**
+   * getProperty
+   *
+   * @param key key
+   * @return Property
+   */
+  public static String getProperty(String key) {
+    return environment.getProperty(key);
+  }
 
-	/**
-	 * @return Environment
-	 */
-	public static Environment getEnvironment() {
-		return environment;
-	}
+  /**
+   * getProperty
+   *
+   * @param key key
+   * @param defaultValue 默认值
+   * @return Property
+   */
+  public static String getProperty(String key, String defaultValue) {
+    return environment.getProperty(key, defaultValue);
+  }
 
-	/**
-	 * @param clazz clazz
-	 * @param <T>   clazz 对应的类型
-	 * @return clazz 对应的Bean
-	 */
-	public static <T> T getBean(Class<T> clazz) {
-		return applicationContext.getBean(clazz);
-	}
+  /**
+   * @param key key
+   * @param targetType targetType
+   * @param <T> target
+   * @return target
+   */
+  public static <T> T getProperty(String key, Class<T> targetType) {
+    return environment.getProperty(key, targetType);
+  }
 
-	/**
-	 * @return 如果当前线程是HttpServletRequest请求返回对应 request,否则返回:null
-	 */
-	public static HttpServletRequest getRequest() {
-		ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-		if (requestAttributes == null) {
-			return null;
-		}
-		return requestAttributes.getRequest();
-	}
+  /**
+   * @param key key
+   * @param targetType targetType
+   * @param defaultValue 默认值
+   * @param <T> target
+   * @return target
+   */
+  public static <T> T getProperty(String key, Class<T> targetType, T defaultValue) {
+    return environment.getProperty(key, targetType, defaultValue);
+  }
 
-	@Override
-	public void setApplicationContext(final ApplicationContext applicationContext) {
-		AutowireHelper.applicationContext = applicationContext;
-		environment = applicationContext.getEnvironment();
-	}
+  /**
+   * getProperty
+   *
+   * @param key key
+   * @return Property
+   */
+  public static String getRequiredProperty(String key) throws IllegalStateException {
+    return environment.getProperty(key);
+  }
+
+  /**
+   * @param key key
+   * @param targetType targetType
+   * @param <T> target
+   * @return target
+   */
+  public static <T> T getRequiredProperty(String key, Class<T> targetType)
+      throws IllegalStateException {
+    return environment.getProperty(key, targetType);
+  }
+
+  /**
+   * @return Environment
+   */
+  public static Environment getEnvironment() {
+    return environment;
+  }
+
+  /**
+   * @param clazz clazz
+   * @param <T> clazz 对应的类型
+   * @return clazz 对应的Bean
+   */
+  public static <T> T getBean(Class<T> clazz) {
+    return applicationContext.getBean(clazz);
+  }
+
+  /**
+   * @return 如果当前线程是HttpServletRequest请求返回对应 request,否则返回:null
+   */
+  public static HttpServletRequest getRequest() {
+    ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder
+        .getRequestAttributes();
+    if (requestAttributes == null) {
+      return null;
+    }
+    return requestAttributes.getRequest();
+  }
+
+  @Override
+  public void setApplicationContext(final ApplicationContext applicationContext) {
+    AutowireHelper.applicationContext = applicationContext;
+    environment = applicationContext.getEnvironment();
+  }
 
 }
